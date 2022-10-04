2a7d9e9a7213b0392f3ebd3a2e48018095ee0ca0
elshiraphine <elsherviana@gmail.com>
2022-10-04 16:45:46 +0700

    feat: add project repository

---
+++ src/main/java/org/binar/repository/StudioRepository.java
@@ -0,0 +1,8 @@
+package org.binar.repository;
+
+import org.binar.model.Studio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudioRepository extends JpaRepository<Studio, Long> {
+
+}
